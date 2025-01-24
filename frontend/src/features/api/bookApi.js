7f2eaddabd2f29183f9a0a4f5984f7524877be8a
Bhavesh <1bhaveshjaswani1@gmail.com>
2025-01-24 17:48:02 +0530

    third commit

--- frontend/src/features/api/bookApi.js
+++ frontend/src/features/api/bookApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const HOST_API = "http://localhost:2020/api/v1/listing";
+const HOST_API = "https://airbnb-2025-1.onrender.com/api/v1/listing";
 
 export const bookApi = createApi({
   reducerPath: "bookApi",
--- frontend/src/features/api/hostApi.js
+++ frontend/src/features/api/hostApi.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
-const HOST_API = "http://localhost:2020/api/v1/host";
+const HOST_API = "https://airbnb-2025-1.onrender.com/api/v1/host";
 
 export const hostApi = createApi({
     reducerPath: "hostApi",
