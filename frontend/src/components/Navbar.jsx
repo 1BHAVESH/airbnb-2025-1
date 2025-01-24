7dbbcbd4f46d9b5ae1d86f712ecfdab4acbbfda2
Bhavesh <1bhaveshjaswani1@gmail.com>
2025-01-24 17:56:24 +0530

    four commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
     <div className='flex items-center justify-between border-b-2 w-full md:px-4 mx-auto h-16  mt-2'>
 
         <div onClick={()=> navigate("/")} className='ml-3 cursor-pointer'>
-        <img className='w-[118px] md:ml-14' src='/public/airbnb (1).png' />
+        <img className='w-[118px] md:ml-14' src='/airbnb (1).png' />
         </div>
 
         
